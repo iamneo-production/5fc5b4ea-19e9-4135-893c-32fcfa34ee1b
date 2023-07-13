67a4ebd86d24f10f7cc8a3464ef4690b7c34c31c
SatyaN24 <santhinalla24@gmail.com>
2023-07-13 13:42:13 +0000

    User Controller

---
+++ dotnetapp/BusinessLayer.cs
@@ -0,0 +1,29 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using dotnetapp.Models;
+using Microsoft.AspNetCore.Http;
+namespace dotnetapp
+{
+    public class BusinessLayer
+    {
+        private DataAccessLayer data_Access_layer = new DataAccessLayer();
+        public string addUser(ProfileModel pm)
+        {
+            return data_Access_layer.addUser(pm);
+        }
+        public ProfileModel getUser(string email)
+        {
+            return data_Access_layer.getUser(email);
+        }
+        public string editUser(string email, ProfileModel data)
+        {
+            return data_Access_layer.editUser(email, data);
+        }
+        public string deleteUser(string email)
+        {
+            return data_Access_layer.deleteUser(email);
+        }
+    }
+}
+
